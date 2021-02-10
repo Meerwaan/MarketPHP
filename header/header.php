2b24dfac7c8acb249d4a5c3adeaab61731d1fe5b
merwan <merwanlaouini@gmail.com>
2021-02-10 01:24:01 +0100

    Debur du Front

---
+++ header/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <a href="accueil.php"><img src="img/homepage.png" alt="logo" class="img"></a>
+
+            </div>
+            
+            <div class="bout">
+                
+                <a href="#">Se connecter</a>
+                <a href="#">Panier</a>
+            </div>
+        </nav>
+    </header>
+    
+</body>
+</html>
