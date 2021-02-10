fdaf440c4e07aa8e1fc60b0a4d0b8cf688646b47
merwan <merwanlaouini@gmail.com>
2021-02-10 11:19:05 +0100

    front fini

--- accueil.php
+++ accueil.php
@@ -3,13 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Accueil</title>
 </head>
 <body >
-<?php 
-require("header/header.php")
-?>
+<header>
+        <nav class="navbar">
+            <div class="logo">
+                <a href="accueil.php"><img src="img/homepage.png" alt="logo" class="img"></a>
+
+            </div>
+            
+            <div class="bout">
+                
+                <a href="#">Se connecter</a>
+                <a href="#">Panier</a>
+            </div>
+        </nav>
+    </header>
+
+<div class="item">
+    <div class="box-item">
+        <p>Produit 1</p>
+        <img class="img1" src="img/fifa21.PNG" alt="">
+        <p>Prix: 80€</p>
+        <p>Dispo </p>
+        <button name="submit">Ajouter au Panier</button>
+    </div>
+    <div class="box-item">
+        <p>Produit 2</p>
+        <img class="img1" src="img/COD.JPG" alt="">
+        <p>Prix: 60€</p>
+        <p>Dispo</p>
+        <button name="submit">Ajouter au Panier</button>
+    </div>
+    <div class="box-item">
+        <p>Produit 3</p>
+        <img class="img1" src="img/WDL.JPG" alt="">
+        <p>Prix: 50€</p>
+        <p>Dispo</p>
+        <button name="submit">Ajouter au Panier</button>
+    </div>
+    <div class="box-item">
+        <p>Produit 4</p>
+        <img class="img1" src="img/ACV.JPG" alt="">
+        <p>Prix: 70€</p>
+        <p>Dispo</p>
+        <button name="submit">Ajouter au Panier</button>
+    </div>
+
+</div>
+
+
 
 
     
