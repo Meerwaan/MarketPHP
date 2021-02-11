5f1f4212ad2a8aea43d73e69d2949e740c12e3b1
Angel <Angelmoreau95@gmail.com>
2021-02-11 15:18:19 +0100

    changement police

--- public/accueil.php
+++ public/accueil.php
@@ -27,6 +27,7 @@ include 'header/header.php';
         <p>Fifa 21</p>
         <img class="img1" src="../img/fifa21.PNG" alt="">
         <p>Prix: 80€</p>
+        <img class="img2" src="../img/stars.PNG" alt="">
         <p>Dispo</p>
         <a ><button name="submit">Ajouter au Panier</button></a>
     </div>
@@ -34,6 +35,7 @@ include 'header/header.php';
         <p>COD Cold War</p>
         <img class="img1" src="../img/COD.JPG" alt="">
         <p>Prix: 60€</p>
+        <img class="img2" src="../img/stars.PNG" alt="">
         <p>Dispo</p>
         <button name="submit">Ajouter au Panier</button>
     </div>
@@ -41,6 +43,7 @@ include 'header/header.php';
         <p>Watch Dog Legion</p>
         <img class="img1" src="../img/WDL.JPG" alt="">
         <p>Prix: 50€</p>
+        <img class="img2" src="../img/stars.PNG" alt="">
         <p>Dispo</p>
         <button name="submit">Ajouter au Panier</button>
     </div>
@@ -48,6 +51,7 @@ include 'header/header.php';
         <p>Ass'Valhalla</p>
         <img class="img1" src="../img/ACV.JPG" alt="">
         <p>Prix: 70€</p>
+        <img class="img2" src="../img/stars.PNG" alt="">
         <p>Dispo</p>
         <button name="submit">Ajouter au Panier</button>
     </div>
@@ -59,6 +63,7 @@ include 'header/header.php';
         <p>PS5</p>
         <img class="img1" src="../img/PS5.JPG" alt="">
         <p>Prix: 490€</p>
+        <img class="img2" src="../img/stars.png" alt="">
         <p>Rupture </p>
         <a ><button name="submit">Ajouter au Panier</button></a>
     </div>
@@ -66,6 +71,7 @@ include 'header/header.php';
         <p>Xbox Series X</p>
         <img class="img1" src="../img/Microsoft-Xbox-Series-X.JPG" alt="">
         <p>Prix: 500€</p>
+        <img class="img2" src="../img/stars.PNG" alt="">
         <p>Rupture</p>
         <button name="submit">Ajouter au Panier</button>
     </div>
@@ -73,6 +79,7 @@ include 'header/header.php';
         <p>Nintendo Switch</p>
         <img class="img1" src="../img/nintendo-switch.JPG" alt="">
         <p>Prix: 320€</p>
+        <img class="img2" src="../img/stars.PNG" alt="">
         <p>Dispo</p>
         <button name="submit">Ajouter au Panier</button>
     </div>
@@ -84,6 +91,7 @@ include 'header/header.php';
         <p>Plasytion Plus</p>
         <img class="img1" src="../img/PsPLus.JPG" alt="">
         <p>Prix: 60€</p>
+        <img class="img2" src="../img/stars.PNG" alt="">
         <p>Dispo</p>
         <a ><button name="submit">Ajouter au Panier</button></a>
     </div>
@@ -91,6 +99,7 @@ include 'header/header.php';
         <p>Xbox Live</p>
         <img class="img1" src="../img/xboxLive.JPG" alt="">
         <p>Prix: 20€</p>
+        <img class="img2" src="../img/stars.png" alt="">
         <p>Dispo</p>
         <button name="submit">Ajouter au Panier</button>
     </div>
@@ -98,6 +107,7 @@ include 'header/header.php';
         <p>Nintendo Online</p>
         <img class="img1" src="../img/abSwitch.JPG" alt="">
         <p>Prix: 60€</p>
+        <img class="img2" src="../img/stars.PNG" alt="">
         <p>Dispo</p>
         <button name="submit">Ajouter au Panier</button>
     </div>
