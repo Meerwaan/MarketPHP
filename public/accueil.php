52eb6bae5f38d2eb53aead731d8d7bfa05813ef2
merwan <merwanlaouini@gmail.com>
2021-02-12 11:44:44 +0100

    Inscription fini

--- public/accueil.php
+++ public/accueil.php
@@ -9,10 +9,8 @@ require_once '../database/database.php';
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link href="header/header.css" rel="stylesheet" />
     <link rel="stylesheet" href="style.css">
     <title>Accueil</title>
 </head>
@@ -98,7 +96,7 @@ include 'header/header.php';
     <div class="box-item">
         <p>Xbox Live</p>
         <img class="img1" src="../img/xboxLive.JPG" alt="">
-        <p>Prix: 20€</p>
+        <p>Prix: 60€</p>
         <img class="img2" src="../img/stars.png" alt="">
         <p>Dispo</p>
         <button name="submit">Ajouter au Panier</button>
@@ -106,7 +104,7 @@ include 'header/header.php';
     <div class="box-item">
         <p>Nintendo Online</p>
         <img class="img1" src="../img/abSwitch.JPG" alt="">
-        <p>Prix: 60€</p>
+        <p>Prix: 20€</p>
         <img class="img2" src="../img/stars.PNG" alt="">
         <p>Dispo</p>
         <button name="submit">Ajouter au Panier</button>
--- public/header/header.php
+++ public/header/header.php
@@ -1,73 +1,18 @@
-<?php
-
-if (isset($_SESSION['auth'])) : ?>
-
-    <?php $user = $_SESSION['auth']; ?>
-
-    <!-- Navbar de l'utilisateur connecté -->
-    <nav class="navbar sticky-top navbar-light navbar-expand-xl bg-white fz-text shadow-sm d-flex align-items-center">
-        <div class="container-fluid">
-            <a class="navbar-brand d-flex align-items-center" href="">
-                <img class="ms-4" src="../../img/logo-bb.png" width="150" alt="" loading="lazy">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse d-flex justify-content-end" style="font-size: 15px" id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0 me-3 d-flex flex-wrap" style="font-weight:400">
-                    <li class="nav-item d-flex flex-column px-lg-3 pt-2 pt-lg-0">
-                        <a class="nav-link text-dark" aria-current="page" href="">Accueil</a>
-                        <span class="border-li bg-danger rounded-pill ms-2"></span>
-                    </li>
-                    <li class="nav-item d-flex flex-column px-lg-3">
-                        <a class="nav-link text-dark" href="#">Panier</a>
-                        <span class="border-li bg-danger rounded-pill ms-2"></span>
-                    </li>
-                    <li class="nav-item d-flex flex-column px-lg-3">
-                        <a class="nav-link text-dark" href="">Profil</a>
-                        <span class="border-li bg-warning rounded-pill ms-2"></span>
-                    </li>
-                    <li class="nav-item d-flex flex-column px-lg-3">
-                        <a class="nav-link text-dark" href="">Se déconnecter</a>
-                        <span class="border-li bg-warning rounded-pill ms-2"></span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-<?php else : ?>
-
-    <!-- Navbar de l'utilisateur non connecté -->
-    <nav class="navbar sticky-top navbar-light navbar-expand-xl bg-white fz-text shadow-sm d-flex align-items-center">
-        <div class="container-fluid">
-            <a class="navbar-brand d-flex align-items-center" href="">
-                <img class="ms-4" src="../../img/logo-bb.png" width="150" alt="" loading="lazy">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse d-flex justify-content-end" style="font-size: 15px" id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0 me-3" style="font-weight:400">
-                    <li class="nav-item d-flex flex-column px-lg-3 pt-2 pt-lg-0">
-                        <a class="nav-link text-dark" aria-current="page" href="../accueil.php">Accueil</a>
-                        <span class="border-li bg-danger rounded-pill ms-2"></span>
-                    </li>
-                    <li class="nav-item d-flex flex-column px-lg-3 pt-2 pt-lg-0">
-                        <a class="nav-link text-dark" aria-current="page" href="connexion/inscription.php">Inscription</a>
-                        <span class="border-li bg-danger rounded-pill ms-2"></span>
-                    </li>
-                    <li class="nav-item d-flex flex-column px-lg-3">
-                        <a class="nav-link text-dark" href="connexion/connexion.php">Connexion</a>
-                        <span class="border-li bg-warning rounded-pill ms-2"></span>
-                    </li>
-                    <li class="nav-item d-flex flex-column px-lg-3">
-                        <a class="nav-link text-dark" href=">Inscription</a>
-                        <span class="border-li bg-warning rounded-pill ms-2"></span>
-                    </li>            
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-<?php endif; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="header.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="../public/accueil.php">Home</a>
+  <a href="../public/connexion/inscription.php">Inscription</a>
+  <a href="../public//.php">Panier</a>
+  
+</div>
+    
+</body>
+</html>
